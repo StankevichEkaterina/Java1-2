5ccf152c6c7dd79a75ce9ed348cbaf0cdf19d08e
Станкевич Екатерина <obuchenie_21_22@mail.ru>
2022-03-16 20:15:58 +0300

    Initianal commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int currentAccountBalance = 2000000000;
+        int transferAmount = 500000000;
+        int remainder = currentAccountBalance + transferAmount;
+
+        System.out.println((remainder) + "Миллиардов рублей");
+    }
+}
+
